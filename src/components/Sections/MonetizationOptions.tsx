a413ddcf716d0f2e54c72ed75ed49bf6adb35037
Dmitriy <bartenevdmitriy@bk.ru>
2023-05-26 13:40:44 +0400

    Fixes

--- src/components/Sections/MonetizationOptions.tsx
+++ src/components/Sections/MonetizationOptions.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
 const MonetizationOptions: React.FC = () => {
-	return (
-		<div className="flex justify-center items-start py-16 px-7 xl:py-28 xl:px-64 max-w-[1440px] mx-auto flex-col sm:flex-row bg-slate-100 sm:bg-white">
-			<h3 className="text-black text-3xl leading-[48px] font-bold max-w-md mr-8">
-				A-ADS provides alternative technologies for{' '}
-				<span className="text-blue">monetizing your traffic</span>
-			</h3>
-			<div className="font-inter text-base font-medium text-lightGray max-w-sm">
-				<p className="mb-5 mt-6 sm:mt-0">
-					In addition to the ability to sell ads on your website directly, you
-					can also earn from the pool of our active advertisers.
-				</p>
-				<p>
-					Our advertising model doesn't limit your earnings by clicks and
-					impressions, but expands it with advertisers' budgets they eager to
-					spend on your website.
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex justify-center items-start py-16 px-7 xl:py-28 xl:px-64 max-w-[1440px] mx-auto flex-col sm:flex-row bg-slate-100 sm:bg-white'>
+      <h3 className='text-black text-3xl leading-[48px] font-bold max-w-md mr-8'>
+        A-ADS provides alternative technologies for{' '}
+        <span className='text-blue'>monetizing your traffic</span>
+      </h3>
+      <div className='font-inter text-base font-medium text-lightGray max-w-sm'>
+        <p className='mb-5 mt-6 sm:mt-0'>
+          In addition to the ability to sell ads on your website directly, you
+          can also earn from the pool of our active advertisers.
+        </p>
+        <p>
+          Our advertising model doesn&rsquo;t limit your earnings by clicks and
+          impressions, but expands it with advertiser&rsquo;s budgets they eager
+          to spend on your website.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MonetizationOptions;
